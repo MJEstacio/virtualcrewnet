7c4c96b51680cf5c2478534b14f636f0cd887745
MJEstacio <mjestacioiii@gmail.com>
2024-04-11 02:22:46 +0800

    change text

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -9,7 +9,7 @@ const CTASection = () => {
     >
       <div className="max-w-7xl mx-auto  flex justify-center flex-col gap-10 px-2 lg:px-0 ">
         <h3 className="text-3xl font-bold   text-center text-white montserrat ">
-          Lorem ipsum dolor sit amet consectetur adipisicing.
+          Unlock Your Time Freedom: Delegate Tasks Today! Let&apos;s talk
         </h3>
         <hr className="w-1/2 mx-auto " />
         <div className="flex justify-center ">
