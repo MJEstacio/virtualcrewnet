144a8586fe636c213e56f601f30d32924b1bc72d
MJEstacio <mjestacioiii@gmail.com>
2024-04-20 12:16:50 +0800

    change the quote to escaped command

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -9,8 +9,8 @@ const CTASection = () => {
     >
       <div className="max-w-7xl mx-auto  flex justify-center flex-col gap-10 px-2 lg:px-0 ">
         <h3 className="text-2xl font-bold   text-center text-white montserrat ">
-          Unlock your business's full potential. Reach out now and Let's start
-          achieving your goals together!
+          Unlock your business&apos;s full potential. Reach out now and
+          Let&apos;s start achieving your goals together!
         </h3>
         <hr className="w-1/2 mx-auto " />
         <div className="flex justify-center ">
