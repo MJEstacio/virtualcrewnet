94a099992b007af6c8008632ae90905cc4a9ac55
MJEstacio <mjestacioiii@gmail.com>
2024-04-20 12:14:30 +0800

    About Page Content

---
+++ src/components/AboutHero.tsx
@@ -0,0 +1,13 @@
+const AboutHero = () => {
+  return (
+    <section>
+      <div className="max-w-7xl mx-auto min-h-[70vh] flex justify-center items-center">
+        <h1 className="text-4xl font-bold p-10 border-y-2 border-black montserrat">
+          About VirtualCrew
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default AboutHero;
