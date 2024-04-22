841bcae3db606141edd2567f44d284dd4a2d430a
MJEstacio <mjestacioiii@gmail.com>
2024-04-22 14:48:19 +0800

    few tweaks for the text

--- src/components/AboutHero.tsx
+++ src/components/AboutHero.tsx
@@ -2,7 +2,7 @@ const AboutHero = () => {
   return (
     <section>
       <div className="max-w-7xl mx-auto min-h-[70vh] flex justify-center items-center">
-        <h1 className="text-4xl font-bold p-10 border-y-2 border-black montserrat">
+        <h1 className="text-3xl sm:text-4xl font-bold p-10 border-y-2 border-black montserrat">
           About VirtualCrew
         </h1>
       </div>
