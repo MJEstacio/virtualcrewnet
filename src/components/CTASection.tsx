efe0b789eff3f66abd01f01b7dc36eb7369d7349
MJEstacio <mjestacioiii@gmail.com>
2024-04-30 02:08:57 +0800

    Remove text

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -8,13 +8,13 @@ const CTASection = () => {
       className="min-h-[40vh] bg-black flex"
     >
       <div className="max-w-7xl mx-auto  flex justify-center flex-col gap-10 px-2 lg:px-0 ">
-        <h3 className="text-3xl font-bold  text-center text-white montserrat tracking-wider max-w-4xl px-2 lg:px-0 ">
+        {/* <h3 className="text-3xl font-bold  text-center text-white montserrat tracking-wider max-w-4xl px-2 lg:px-0 ">
           Let&apos;s turn your Dreams into Reality and bringing your Visions to
           Life!
-          {/* Unlock your business&apos;s full potential. Reach out now and
+        </h3> */}
+        {/* Unlock your business&apos;s full potential. Reach out now and
           Let&apos;s start achieving your goals together! */}
-        </h3>
-        <hr className="w-1/2 mx-auto " />
+        {/* <hr className="w-1/2 mx-auto " /> */}
         <div className="flex justify-center ">
           <CTA
             href="/contact"
