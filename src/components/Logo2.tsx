a10d55bfa7f40080c77ef6bddabf046e5b87635e
MJEstacio <mjestacioiii@gmail.com>
2024-06-13 11:59:12 +0800

    Logo top nav

---
+++ src/components/Logo2.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Logo2 = () => {
+  return (
+    <Link href={"/"}>
+      <Image
+        src={"/Logo2.jpg"}
+        width={500}
+        height={500}
+        className="w-32"
+        alt="Logo"
+        priority
+      />
+    </Link>
+  );
+};
+
+export default Logo2;
