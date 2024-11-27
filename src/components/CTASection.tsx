8b51794cc3e1c938e6a353fc6004476e34702eca
MJEstacio <mjestacioiii@gmail.com>
2024-11-27 18:06:51 +0800

    add contact and map UI

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -7,11 +7,11 @@ const CTASection = () => {
       className="min-h-[40vh] bg-black flex"
     >
       <div className="max-w-7xl mx-auto  flex justify-center flex-col gap-10 px-2 lg:px-0 ">
-        <h3 className="text-3xl text-center text-white font-mono tracking-wider max-w-4xl px-2 lg:px-0  leading-10 font-serif ">
+        <h2 className="text-3xl text-center text-white  tracking-wider max-w-4xl px-2 lg:px-0  leading-10  ">
           Let&apos;s s bring your Visions to Life. Together,
           <br />
           We&apos;ll realize your Vision
-        </h3>
+        </h2>
         {/* Unlock your business&apos;s full potential. Reach out now and
           Let&apos;s start achieving your goals together! */}
         {/* <hr className="w-1/2 mx-auto " /> */}
