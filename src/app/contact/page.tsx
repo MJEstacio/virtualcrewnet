69600d2623aea52188de244838b793cc77bbb4f6
MJEstacio <mjestacioiii@gmail.com>
2025-01-21 18:49:50 +0800

    remove the map and office

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -25,18 +25,11 @@ const ContactPage = () => {
         <div className="border border-black mt-10 max-w-7xl mx-auto"></div>
         <div className="max-w-7xl mx-auto my-10">
           <div className="mb-5 text-center">
-            <h2 className="text-4xl ">Our Office</h2>
+            {/* <h2 className="text-4xl ">Our Office</h2>
             <p className="text-gray-500">
               1303, US 127 South, Suite 104 #1002, Frankfurt, KY 40601
-            </p>
+            </p> */}
           </div>
-          <iframe
-            className="p-2 rounded-md bg-red-500"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1568.4070575167912!2d-84.89843516108867!3d38.16775940173898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x884274fe6aab246f%3A0xa0f14a9710f564b6!2s1303%20U.S.%20127%20S%20104%201002%2C%20Frankfort%2C%20KY%2040601%2C%20USA!5e0!3m2!1sen!2sph!4v1732701183779!5m2!1sen!2sph"
-            width="100%"
-            height="450"
-            loading="lazy"
-          ></iframe>
         </div>
         <iframe
           className="w-full h-full border-none rounded text-white"
